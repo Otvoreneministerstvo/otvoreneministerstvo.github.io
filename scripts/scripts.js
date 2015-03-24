0e800327f8341d34c03a11d7c1ff139205979a59
Milos Blasko <blaskomm@gmail.com>
2015-03-24 07:50:28 +0100

    initial commit

---
+++ scripts/scripts.js
@@ -0,0 +1,14 @@
+$("#mc-form").ajaxChimp({
+    url: 'http://miloshes.us8.list-manage.com/subscribe/post?u=77417d11fe3832ef806ac3af9&amp;id=6658a9e66e',
+    language: 'sk'
+});
+
+$.ajaxChimp.translations.sk = {
+    'submit': 'Odosielam...',
+    0: 'Potvrdzujúci email odoslaný',
+    1: 'Prosím zadajte emailovú adresu',
+    2: 'Emailová adresa musí obsahovať @',
+    3: 'Za znakom @ musí nasledovať doména',
+    4: 'Pred znakom @ musí nasledovať názov email konta',
+    5: 'Zadaný email je neplatný. Prosím vložte inú email adresu'
+}
